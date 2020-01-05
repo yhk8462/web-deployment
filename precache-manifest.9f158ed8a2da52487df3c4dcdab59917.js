9cbd4a160a0202e4f6472f76aa5a94a4681be93f
yhk8462 <s3726115@rmit.edu.vn>
2020-01-05 16:32:58 +0700

    add

---
+++ precache-manifest.9f158ed8a2da52487df3c4dcdab59917.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5690343da10299b84604a7e3ab7c9f7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ceb83173c4616058f538",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "919b09d65e72657ebc58",
+    "url": "/static/css/main.5f3ec93c.chunk.css"
+  },
+  {
+    "revision": "ceb83173c4616058f538",
+    "url": "/static/js/2.7f311e64.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.7f311e64.chunk.js.LICENSE"
+  },
+  {
+    "revision": "919b09d65e72657ebc58",
+    "url": "/static/js/main.d545a91a.chunk.js"
+  },
+  {
+    "revision": "177d52efa2c7db582a05",
+    "url": "/static/js/runtime-main.5b50bb1e.js"
+  }
+]);
